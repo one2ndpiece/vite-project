5e03d17d3b978a446fd5405d27aa0fd63cf3f10c
YutoTonami <tonami.yuto@unkman.jp>
2024-07-07 12:04:18 +0000

    unk

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button } from "@/components/ui/button"
 import './App.css'
 
 function App() {
@@ -35,6 +36,9 @@ function App() {
         <br />
         unkman
       </p>
+      <div>
+        <Button>Click me</Button>
+      </div>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"  // Added
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES
+    ? "/vite-project/"  // リポジトリ名に合わせて設定
+    : "./",
   plugins: [react()],
+  resolve: {  // Added
+    alias: {  // Added
+      "@": path.resolve(__dirname, "./src"),  // Added
+    },  // Added
+  },  // Added
+  // build: {
+  //   outDir: 'dist'
+  // },
   server: {
     host: '0.0.0.0',
     port: 5173
